7db738be31b164d2830b69b216d0b4bfba14b32e
EmilD-Skillio <emil.dewald@skillio.ai>
2025-10-20 14:40:45 +0300

    trying to get a conflict

--- src/code/main.py
+++ src/code/main.py
@@ -1,5 +1,6 @@
 def pullRequestDemo():
     print("This is a pull request demo. Emil made some things here.")
+    print("Is there a conflict?")
     
 if __name__ == "__main__":
     pullRequestDemo()
